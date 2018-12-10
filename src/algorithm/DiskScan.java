1859a43c420579c1659835d61bed5d1a1002eb1d
hiyongrui <172808H@mymail.nyp.edu.sg>
2018-12-10 21:01:32 +0800

    added more commentszzz

--- src/algorithm/DiskScan.java
+++ src/algorithm/DiskScan.java
@@ -35,9 +35,9 @@ public class DiskScan {
         printSequence("SCAN", location); // calculate the distance, print out
     }
 
-    //Sequence= 86,1470,913,1774,948,1509,1022,1750,130
+    //Original Sequence= 86,1470,913,1774,948,1509,1022,1750,130
     private ArrayList<Integer> arrangeBySCAN(int totalCylinder, int previous, int current, int[] sequence) {
-        // current is 143, previous 125, so increasing 143, 913, 948, 1022, 1470, 1509, 1750, 1774, 5000, 130, 86
+
         System.out.println("inside arrange by scan");
 
         System.out.println("starting array unsorted " + Arrays.toString(sequence)+ "\n");
@@ -51,14 +51,14 @@ public class DiskScan {
                 if ( (current - sequence[i]) < 0) { //143 - 1470 < 0, current = 143, second number is 1470
                     System.out.println("distance > 0 value = " + sequence[i]);
                     emptyRight.add(sequence[i]);
-                    Collections.sort(emptyRight);
+                    Collections.sort(emptyRight); //sort emptyRight array in ascending order
                     //empty.add(4999);
                 }
                 else{ //143 - 86 < 0, current = 143, first number is 86
                     System.out.println("distance > 0 left array = " + sequence[i]);
                     emptyLeft.add(sequence[i]);
-                    Collections.sort(emptyLeft);
-                    Collections.reverse(emptyLeft); //reverse so that its [130,86] instead of [86,130]
+                    Collections.sort(emptyLeft); //sort emptyLeft array in acsending order
+                    Collections.reverse(emptyLeft); //reverse so that its descending [130,86] instead of [86,130]
                 }
             }
             // else if distance is < 0, decreasing to 0
@@ -67,13 +67,13 @@ public class DiskScan {
                 if ( (current - sequence[i]) > 0) {
                     System.out.println("more than 0 value = " + sequence[i]);
                     emptyRight.add(sequence[i]);
-                    Collections.sort(emptyRight);
-                    Collections.reverse(emptyRight);
+                    Collections.sort(emptyRight); //sort emptyRight array in ascending order
+                    Collections.reverse(emptyRight); //reverse so that its descending [86,0]
                 }
                 else{
                     System.out.println("lesser than 0 value =  " + sequence[i]);
                     emptyLeft.add(sequence[i]);
-                    Collections.sort(emptyLeft);
+                    Collections.sort(emptyLeft); //sort emptyLeft array in ascending order
                 }
             }
 
@@ -87,14 +87,14 @@ public class DiskScan {
         System.out.println("\n END OF FOR LOOP RIGHT ARRAY " + emptyRight);
         System.out.println("END OF FOR LOOP LEFT ARRAY " + emptyLeft);
 
-        emptyRight.addAll(emptyLeft); // combine the two arrays together
+        emptyRight.addAll(emptyLeft); // combine the two sorted arrays together
 
         System.out.println("\n originally unsorted = " + Arrays.toString(sequence) );
         System.out.println("finally scan sorted = " + emptyRight  + "\n");
 
-        // Sequence= 86,1470,913,1774,948,1509,1022,1750,130
+        // Original unsorted Sequence = 86,1470,913,1774,948,1509,1022,1750,130
         //End result (1) Scan going to 4999 = [913, 948, 1022, 1470, 1509, 1750, 1774, 4999, 130, 86]
-        //End result (2) Scan going to 0 = [130, 86, 0, 948, 1022, 1470, 1509, 1750, 1774, 913]
+        //End result (2) Scan going to 0 = [86, 0, 130, 913, 948, 1022, 1470, 1509, 1750, 1774]
         return emptyRight; //return sorted array scan
     } //end of arrangeByScan()
 
